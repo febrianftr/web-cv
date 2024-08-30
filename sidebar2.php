5d7864f3addacbfab9756e5f88e8066784fd4ba6
febrianftr <skylinesxfebrian@gmail.com>
2024-08-30 10:04:46 +0700

    2

--- sidebar2.php
+++ sidebar2.php
@@ -14,38 +14,44 @@
 <body>   
     <nav>
         <div class="social-icons">
-            <div class="tooltip-container">
-                <span class="tooltip">About Me</span>
-                <span class="text">
-                    <div class="borde-back">
-                        <div class="icon">
-                            <img src="assets/me.svg" style="width: 30px;">
+            <a href="#my-bio">
+                <div class="tooltip-container">
+                    <span class="tooltip">About Me</span>
+                    <span class="text">
+                        <div class="borde-back">
+                            <div class="icon">
+                                <img src="assets/me.svg" style="width: 30px;">
+                            </div>
                         </div>
-                    </div>
-                </span>
-            </div>
+                    </span>
+                </div>
+            </a>
 
-            <div class="tooltip-container">
-                <span class="tooltip">Education</span>
-                <span class="text">
-                    <div class="borde-back">
-                        <div class="icon">
-                            <img src="assets/education.svg" style="width: 30px;">
+            <a href="#my-ex">
+                <div class="tooltip-container">
+                    <span class="tooltip">Experience</span>
+                    <span class="text">
+                        <div class="borde-back">
+                            <div class="icon">
+                                <img src="assets/education.svg" style="width: 30px;">
+                            </div>
                         </div>
-                    </div>
-                </span>
-            </div>
+                    </span>
+                </div>
+            </a>
 
+           <a href="#my-edu">
             <div class="tooltip-container">
-                <span class="tooltip">Contact Me</span>
-                <span class="text">
-                    <div class="borde-back">
-                        <div class="icon">
-                            <img src="assets/contact.svg" style="width: 30px;">
+                    <span class="tooltip">Education</span>
+                    <span class="text">
+                        <div class="borde-back">
+                            <div class="icon">
+                                <img src="assets/contact.svg" style="width: 30px;">
+                            </div>
                         </div>
-                    </div>
-                </span>
-            </div>
+                    </span>
+                </div>
+           </a>
 
             <!-- <div class="icon-feb fb">
                 <img src="assets/me.svg">
@@ -62,49 +68,80 @@
     <div class="container-fluid">
         <div class="row" style="display: inherit;">
             <div class="back-header"></div>
-            <div class="body-feb">
-                <div class="content-feb">
-                    <img src="assets/feb-2.jpg" class="rounded-circle" alt="Rounded Image">
+            <div id="my-bio">
+                <div class="body-feb">
+                    <div class="content-feb">
+                        <img src="assets/feb-2.jpg" class="rounded-circle" alt="Rounded Image">
+                    </div>
                 </div>
-            </div>
-            <div class="body-feb text-header">
-                    <h1>
-                    FEBRIAN 
-                    <b>FATURAHMAN</b>
-                    </h1>
-            </div>
-            <div class="body-feb text-title">
-                    <h3>
-                        Web Designer
-                    </h3>
-            </div>
-            <div class="body-feb" style="padding-top: 15px;">
-                <p class="text-about">
-                    I am a graduate in Information Systems with experience working as a Web Designer at a medical equipment company.
-                </p>
-            </div>
-            <div class="body-feb" style="padding-top: 20px;">
-                <button class="cta">
-                    <span>Read More</span>
-                    <img src="assets/arrow-right.svg">
-                </button>
+                <div class="body-feb text-header">
+                        <h1>
+                        FEBRIAN 
+                        <b>FATURAHMAN</b>
+                        </h1>
+                </div>
+                <div class="body-feb text-title">
+                        <h3>
+                            Web Designer
+                        </h3>
+                </div>
+                <div class="body-feb" style="padding-top: 15px;">
+                    <p class="text-about">
+                        I am a graduate in Information Systems with experience working as a Web Designer at a medical equipment company.
+                    </p>
+                </div>
+                <div class="body-feb" style="padding-top: 20px;">
+                    <button class="cta">
+                        <span>Read More</span>
+                        <img src="assets/arrow-right.svg">
+                    </button>
+                </div>
             </div>
 
-            <div class="body-feb text-header div-ex">
-                <div class="div-ex-2">
-                    <h1 class="h1-ex">
-                        My <br>
-                        <b>Experi</b><br>
-                        <b>ence</b>
-                    </h1>
-                </div>
-                <div class="p-ex">
-                    <p>
-                        In this role, I was not only responsible for developing and designing websites but also actively involved in product presentations, creating brochures, and conducting product training for clients and internal teams. With a strong combination of technical skills and communication abilities, I am eager to contribute effectively to your team.
-                    </p>
-                    <button class="btn btn-ex">Read More</button>
+            <div id="my-ex">
+                <div class="body-feb text-header div-ex">
+                    <div class="div-ex-2">
+                        <h1 class="h1-ex">
+                            My <br>
+                            <b>Experi</b><br>
+                            <b>ence</b>
+                        </h1>
+                    </div>
+                    <div class="p-ex">
+                        <p>
+                            In this role, I was not only responsible for developing and designing websites but also actively involved in product presentations, creating brochures, and conducting product training for clients and internal teams. With a strong combination of technical skills and communication abilities, I am eager to contribute effectively to your team.
+                        </p>
+                        <button class="btn btn-ex">Read More</button>
+                    </div>
                 </div>
             </div>
+
+            <div id="my-edu">
+                <div class="body-feb text-header div-ex wrap-edu">      
+                    <div class="div-ex-2">
+                        <div class="wrap-img-edu">
+                            <h1>Education</h1>
+                            <img class="img-edu" src="assets/img-edu.jpg">
+                        </div>
+                    </div>
+                    <div class="p-ex" style="position: relative;">
+                        <div class="div-edu">
+                            <h4>Bachelor of Information Systems</h4>
+                            <span>Gunadarma University</span>
+                            <p>Graduated: 2017</p>
+                            <button class="btn btn-edu">Read More</button>
+                        </div>
+                        
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
+            <br><br><br>
         </div>
     </div>
     
